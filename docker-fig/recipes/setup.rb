235a533d7cf29270a7a7ac13f888404968b062b8
min <liao@oneofthem.jp>
2015-01-01 21:02:53 +0900

    Fix:Fixing

--- docker-fig/recipes/setup.rb
+++ docker-fig/recipes/setup.rb
@@ -3,3 +3,7 @@ execute "fig-install" do
     command "curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig"
 end
 
+execute "dockerize-install" do
+    not_if { ::File.exists?("/usr/local/bin/dockerize")}
+    command "curl -L https://github.com/jwilder/dockerize/releases/download/v0.0.1/dockerize-linux-amd64-v0.0.1.tar.gz | tar xz -C /usr/local/bin"
+end
