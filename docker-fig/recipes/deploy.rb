1bb43ae6fe8ea3d6973f38ddc6085c89fb396873
min <liao@oneofthem.jp>
2015-04-01 18:15:00 +0900

    Fix:Fixing

--- docker-fig/recipes/deploy.rb
+++ docker-fig/recipes/deploy.rb
@@ -47,77 +47,40 @@ node[:deploy].each do |application, deploy|
     environment OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables])
   end
 
-  node.set["TMP_CURRENT_HASH"] = ""
-  node.set["TMP_CURRENT_FILE"] = ""
-  b = ruby_block "get current hash" do
+  ruby_block "get current hash" do
+    only_if { layer == 'docker_web' and layer == deploy[:environment_variables][:layer]} 
     block do
       TMP_CURRENT_HASH = `cd #{deploy[:deploy_to]}/current/ && git rev-parse HEAD`
-      node.override["TMP_CURRENT_HASH"] = TMP_CURRENT_HASH.strip 
-      node.override["TMP_CURRENT_FILE"] = "#{TMP_CURRENT_HASH.strip}_app.tgz"
-      puts "The last line is #{node[:TMP_CURRENT_HASH]}"
-      puts "The last file is #{node[:TMP_CURRENT_FILE]}"
+      ENV["TMP_CURRENT_HASH"] = TMP_CURRENT_HASH.strip 
+      ENV["TMP_CURRENT_FILE"] = "#{TMP_CURRENT_HASH.strip}_app.tgz"
+      puts "The last line is #{ENV['TMP_CURRENT_HASH']}"
+      puts "The last file is #{ENV['TMP_CURRENT_FILE']}"
     end
+    notifies :run, "execute[init s3 config]", :immediately
   end
-  b.run_action(:create)
   
   execute "init s3 config" do
     only_if { layer == 'docker_web' and layer == deploy[:environment_variables][:layer]} 
     cwd "/root/"
     command "echo '[default]' > .s3cfg && echo access_key=$AWS_KEY_ID >> .s3cfg && echo secret_key=$AWS_SEC_KEY >> .s3cfg"
     environment OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables])
+    notifies :run, "execute[download app image]", :immediately
   end
 
   execute "download app image" do
-    only_if { layer == 'docker_web' and layer == deploy[:environment_variables][:layer] and !node[:TMP_CURRENT_HASH].empty?} 
+    only_if { layer == 'docker_web' and layer == deploy[:environment_variables][:layer]} 
     cwd "/root/"
-    command "s3cmd get s3://#{deploy[:environment_variables][:AWS_S3_BUCKET]}/images/$TMP_CURRENT_FILE ./$TMP_CURRENT_FILE --continue"
-    environment "TMP_CURRENT_FILE" => "#{node[:TMP_CURRENT_FILE]}"
+    command "s3cmd get s3://#{deploy[:environment_variables][:AWS_S3_BUCKET]}/images/$TMP_CURRENT_FILE ./$TMP_CURRENT_FILE --force"
+    #icommand 'echo "s3cmd get s3://#{deploy[:environment_variables][:AWS_S3_BUCKET]}/images/$TMP_CURRENT_FILE ./$TMP_CURRENT_FILE --continue" > /tmp/xxx'
+    action :nothing
+    notifies :run, "execute[load app image]", :immediately
   end
 
   execute "load app image" do
     only_if { layer == 'docker_web' and layer == deploy[:environment_variables][:layer]} 
     cwd "/root/"
     command "gunzip -c $TMP_CURRENT_FILE | docker load"
-    environment "TMP_CURRENT_FILE" => "#{node[:TMP_CURRENT_FILE]}"
+    action :nothing
   end
 end
 
-include_recipe "docker-fig::create_env_file"
-
-fig_origin_dir = '/srv/www/docker/current'
-fig_work_dir = '/root/docker'
-
-execute "init-docker-fig-dir" do
-    cwd "#{fig_origin_dir}"
-    command "mkdir -p #{fig_work_dir}; cp -a #{fig_origin_dir}/* #{fig_work_dir}/"
-end
-
-directory "#{fig_work_dir}/app/" do
-  only_if { layer == 'docker_web'} 
-  #user deploy[:user]
-  #group deploy[:group]
-  mode '0755'
-  action :create
-end
-
-#execute "fig-build-fluentd" do
-#    cwd "#{fig_work_dir}"
-#    command "fig build fluentd"
-#end
-
-#execute "fig-run-fluentd" do
-#    cwd "#{fig_work_dir}"
-#    command "fig up -d logspout"
-#end
-
-execute "fig-run-web" do
-    only_if { layer == 'docker_web'} 
-    cwd "#{fig_work_dir}"
-    command "fig up -d app web"
-end
-
-execute "fig-run-db" do
-    only_if { layer == 'docker_db'} 
-    cwd "#{fig_work_dir}"
-    command "fig up -d db"
-end
